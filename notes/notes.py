a301794ae788e93b674f106a79817d1fafb1ed33
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-04-03 08:05:18 +0530

    basics

---
+++ notes/notes.py
@@ -0,0 +1,36 @@
+# --------------------------------------------------LECTURE 1-----------------------------------------------------
+
+
+# -------PYTHON-------(it internally codes in c language)
+# dnamically typed language
+# compiler and interpreter
+# 1.python uses interpretor
+# 2.one source and freeware 
+# 3. cpython use karne se time kam lagta hai 
+# 4.also called general purpose programming lang.
+
+# ------keyword-------
+# 1.these are the reserved words whose meaning is predefined 
+
+# Identifiers:- name given to entitoes like variable,class,function..
+
+
+# RULE OF IDENTIFIERS:- 
+# 1.we can use A-Z,a-z,0-9,_
+# 2.we cannot use a name starting with digit
+# 3.case-sensitive
+# 4. keywords ko as identifiers use nhi kar sakte
+
+# VARIABLE -----
+# container used to store data
+# two or more variable with same value will have same address.
+# if we change the value of any variable its datatype will also change
+
+# heap ke andar value store karta hai stack ke andar reference store karta hai..
+
+# address check karne ke liye we use id
+# python me string use hota hai as input and dusre use karna hai to use convert karna padta haii...
+
+# pythin me char datatype nhi hota haii..
+# True✅  true
+# False ✅    false
---
+++ notes/notes2.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------LECTURE2-------------------------------- 
+
+
+# ---------------OPERATORS-------------------
+# arithmatic-+,-,//,%,**
+# ----------------FLOW OPERATOR--------------------
+# 7//5 =1 dono value int hai to value int aayegi
+# (5 one ja 5 hai to 1 aaya hai)
+# 12//20 = 0 
+# 7.2//5 == 1.0 in flow division
+# normal division me 7.2/5 = 1.44 vahan .44 0 ho jaayega
+# --------------exponent operator-------------
+# 5 base 3 = 125
+# 10%4=2 
+# ------------relational operator------------
+# <=,>=,== etc
+# ---------------logical operator------------
+# and , or,not
+# -------------conditional statement-------------
+# python me : colon use hota hai instead of bracket
+# if(condtion) ❌ 
+# if condition :✅ 
+# we use elif✅ instead of else if ❌
