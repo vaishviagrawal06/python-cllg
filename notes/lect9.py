229500e33b25d777a427c40ee93a3d3cb60ae6e9
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-05-11 12:51:39 +0530

    python output, datatypes, variable, user input, type conversion, literals

---
+++ notes/lect9.py
@@ -0,0 +1,8 @@
+# --------------SET----------------
+# unique value store
+# only immutable elements store->int,float,uple,boolean
+# mutable
+# unordered hota haiii
+# koi bhi indexing nhi hoti haii
+# stored in {}
+# existing element ko update nhii kar sakte 
